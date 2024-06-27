0149650eb948be2b790dcb4b0b61ac5e85a8685f
alinaitschool <alinaghetler4@gmail.com>
2024-06-27 13:37:51 +0200

    Add the 9'th challenge and the session 7 practice

---
+++ src/main/java/com/github/alina/session_6_java_core_api/challenge/Exercise_9.java
@@ -0,0 +1,34 @@
+package com.github.alina.session_6_java_core_api.challenge;
+
+import java.util.HashMap;
+
+/*9. Frequency Counter
+        Description: Using StringBuilder, write a program that counts the frequency of each character in a string.
+*/
+public class Exercise_9 {
+    public static void main(String[] args) {
+        String input = "example stringgg"; // Example input string
+
+        // Function call to count the frequency of characters
+        int[] frequencyArray = countCharacterFrequency(input);
+
+        // Print the frequency of each character
+        for (int i = 0; i < frequencyArray.length; i++) {
+            if (frequencyArray[i] > 0) {
+                System.out.println("Character: " + (char) i + ", Frequency: " + frequencyArray[i]);
+            }
+        }
+    }
+
+    public static int[] countCharacterFrequency(String input) {
+        int[] frequencyArray = new int[256]; // Array to store the frequency of each ASCII character
+        StringBuilder sb = new StringBuilder(input);
+
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            frequencyArray[c]++;
+        }
+
+        return frequencyArray;
+    }
+}
---
+++ src/main/java/com/github/alina/session_7_utillity_classes/practice/ObjectPractice.java
@@ -0,0 +1,15 @@
+package com.github.alina.session_7_utillity_classes.practice;
+
+public class ObjectPractice {
+    public static void main(String[] args) {
+        Object object = new Object();
+
+        Car car = new Car();
+        Object secondObject = car;
+    }
+}
+
+class Car {
+
+}
+
