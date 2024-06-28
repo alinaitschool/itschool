8dfc2c0d072636c506cccba12af3801d8cfd2b44
alinaitschool <alinaghetler4@gmail.com>
2024-06-28 17:10:17 +0200

    Add 10 Challenge

--- src/main/java/com/github/alina/session_6_java_core_api/challenge/Exercise_9.java
+++ src/main/java/com/github/alina/session_6_java_core_api/challenge/Exercise_9.java
@@ -5,27 +5,22 @@ package com.github.alina.session_6_java_core_api.challenge;
 */
 public class Exercise_9 {
     public static void main(String[] args) {
-        String input = "example stringgg"; // Example input string
-
-        // Function call to count the frequency of characters
+        String input = "example stringgg";
         int[] frequencyArray = getCharacterFrequency(input);
-
-        // Print the frequency of each character
         for (int i = 0; i < frequencyArray.length; i++) {
             if (frequencyArray[i] > 0) {
                 System.out.println("Character: " + (char) i + ", Frequency: " + frequencyArray[i]);
             }
         }
     }
-    public static int[] getCharacterFrequency(String input) {
-        int[] frequencyArray = new int[256]; // Array to store the frequency of each ASCII character
-        StringBuilder sb = new StringBuilder(input);
 
+    public static int[] getCharacterFrequency(String input) {
+        int[] frequencyArray = new int[256];
+        StringBuilder sb = new StringBuilder(input);
         for (int i = 0; i < sb.length(); i++) {
             char c = sb.charAt(i);
             frequencyArray[c]++;
         }
-
         return frequencyArray;
     }
 }
