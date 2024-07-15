3a56e9c4c0db260f85846e637c37c4cf2b2d4d8b
alinaitschool <alinaghetler4@gmail.com>
2024-07-15 13:13:49 +0200

    Add  sessiune 11

---
+++ src/main/java/com/github/alina/session_7_utillity_classes/practice/MathPractice.java
@@ -0,0 +1,16 @@
+package com.github.alina.session_7_utillity_classes.practice;
+
+public class MathPractice {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 23;
+
+        int minValue = Math.min(firstNumber, secondNumber);
+        int maxValue = Math.max(firstNumber, secondNumber);
+
+        System.out.println("Min value: " + minValue);
+        System.out.println("Max value: " + maxValue);
+
+        System.out.println(Math.PI);
+    }
+}
