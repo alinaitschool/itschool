e43c2ab19b3b6aacb17adc06b1c474c9e3756f33
alinaitschool <alinaghetler4@gmail.com>
2024-07-16 18:04:59 +0200

    Change solution for challenge 1 since for implementation is required array and not arraylist

--- src/main/java/com/github/alina/session_8_array_linkedlist/challenge/Challenge_01.java
+++ src/main/java/com/github/alina/session_8_array_linkedlist/challenge/Challenge_01.java
@@ -1,21 +1,13 @@
 package com.github.alina.session_8_array_linkedlist.challenge;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 //1. Create a shopping list with Array and print the values
 public class Challenge_01 {
     public static void main(String[] args) {
-      System.out.println(createShoppingList());
+        String[] shoppingList = {"Butter", "Milk", "Bread", "Cucumber"};
+        for(String product: shoppingList){
+            System.out.println(product);
+        }
     }
-    public static List createShoppingList(){
-        ArrayList<String> shoppingList = new ArrayList<>();
-        shoppingList.add("Milk");
-        shoppingList.add("Cream");
-        shoppingList.add("Butter");
-        shoppingList.add("Sparangel");
-        return shoppingList;
-    }
-
 }
