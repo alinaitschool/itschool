4edd678e982161e6938b630f4c35ec77b40b8c2a
alinaitschool <alinaghetler4@gmail.com>
2024-07-16 20:26:33 +0200

    Change solution for challenge 2 session 8

--- src/main/java/com/github/alina/session_8_array_linkedlist/challenge/Challenge_01.java
+++ src/main/java/com/github/alina/session_8_array_linkedlist/challenge/Challenge_01.java
@@ -1,12 +1,11 @@
 package com.github.alina.session_8_array_linkedlist.challenge;
 
 
-
 //1. Create a shopping list with Array and print the values
 public class Challenge_01 {
     public static void main(String[] args) {
         String[] shoppingList = {"Butter", "Milk", "Bread", "Cucumber"};
-        for(String product: shoppingList){
+        for (String product : shoppingList) {
             System.out.println(product);
         }
     }
