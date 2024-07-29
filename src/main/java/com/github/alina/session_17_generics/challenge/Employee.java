12e69a79e65abdd4b8ada5c43ab8b0785ef7f007
alinaitschool <alinaghetler4@gmail.com>
2024-07-29 18:41:38 +0200

    Add session 17, hashcode and equal method

---
+++ src/main/java/com/github/alina/session_17_generics/challenge/Employee.java
@@ -0,0 +1,36 @@
+package com.github.alina.session_17_generics.challenge;
+
+import java.util.Objects;
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String  secondName;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Employee(long id, String firstName, String secondName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return getId() == employee.getId() && Objects.equals(getFirstName(), employee.getFirstName()) && Objects.equals(secondName, employee.secondName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), secondName);
+    }
+}
---
+++ src/main/java/com/github/alina/session_17_generics/challenge/EmployeeTest.java
@@ -0,0 +1,14 @@
+package com.github.alina.session_17_generics.challenge;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(1L, "Alina", "Ghetler");
+        Employee emp2 = new Employee(1L, "Alina", "Ghetler");
+
+        System.out.println("emp1 equal emp2 " + emp1.equals(emp2));
+        Set<Employee> employeeSet= new HashSet<>();
+    }
+}
